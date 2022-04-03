8c66828f8a2ca8c4abc5b240a07b1c126b7a1901
tien <460592@student.saxion.nl>
2022-04-03 18:49:27 +0200

    initial and working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="main">
+    <h1>List of facts</h1>
+    <v-simple-table dark>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th class="text-left">
+                Info
+              </th>
+              <th class="text-left">
+                Source
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="item in messages"
+              :key="item.id"
+            >
+              <td>{{ item.info }}</td>
+              <td>{{ item.source }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
   </div>
 </template>
 
 <script>
+let sseClient;
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
+  },
+  data() {
+    return {
+      messages: []
+    }
+  },
+  mounted() {
+    sseClient = this.$sse.create({
+        url: 'http://localhost:3000/events',
+        format: 'json',
+        polyfill: true,
+      });
+      sseClient.on('message', this.handleMessage);
+      sseClient.connect()
+      .then(sse => {
+        console.log('We\'re connected! SSe ', sse);
+      })
+      .catch((err) => {
+        console.error('Failed to connect to server', err);
+      });
+  },
+  methods: {
+    handleMessage(message) {
+      if(Array.isArray(message)){
+        this.messages = message;
+      } else {
+        this.messages.push(message);
+      }
+    },
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.main {
+  margin-left: 20px;
+  margin-top: 10px;
+  margin-right: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSSE from 'vue-sse';
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+Vue.use(VueSSE);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
